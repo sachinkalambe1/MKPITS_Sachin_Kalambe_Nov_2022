ed82ed931e8546f8b6356bdfd9e2518b2270cc7d
sachinkalambe1 <118258632+sachinkalambe1@users.noreply.github.com>
2023-01-23 23:16:47 +0530

    Area of Square By using java

---
+++ AreaOf_Square.java
@@ -0,0 +1,30 @@
+class formula1
+{
+int s=5;
+ float AreaS;
+
+void calculate()
+{
+
+AreaS=s*s;
+}
+
+void display()
+{
+
+System.out.println(AreaS);
+}
+}
+
+
+public class AreaOf_Square
+{
+public static void main(String[] args)
+{
+formula1 s=new formula1();
+ s.calculate();
+ s.display();
+
+}
+}
+
