c3a3fff1b219cc23125dcd21e473d805113f8014
sachinkalambe1 <118258632+sachinkalambe1@users.noreply.github.com>
2023-01-23 23:20:31 +0530

    Array Demo By using java

---
+++ ArrayDemo.java
@@ -0,0 +1,22 @@
+class Array
+{
+
+void Arr()
+{
+
+int a[]={1,2,3,4};
+ for(int i=0;i<=3;i++)
+ {
+ System.out.println(a[i]);
+ }
+}
+}
+class ArrayDemo
+{
+public static void main(String[] args)
+{
+ Array s1=new Array();
+ s1.Arr();
+
+}
+}
