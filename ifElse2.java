5d77e93d4f7517767d78e300bc9382cc0b024f3e
sachinkalambe1 <118258632+sachinkalambe1@users.noreply.github.com>
2023-01-25 10:49:08 +0530

    ifelse 2 by  java

---
+++ ifElse2.java
@@ -0,0 +1,14 @@
+class ifElse2
+{
+public static void main(String[] args)
+{
+int age=17;
+if(age>=18)
+{
+System.out.println("Eligible to donate vote");
+}
+else
+{
+System.out.println("Not Eligible to donate vote");
+}
+}}
