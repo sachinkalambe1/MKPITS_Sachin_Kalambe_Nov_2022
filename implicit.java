6b9088f52bd63ae927ac95580def61b1ec611d8b
sachinkalambe1 <118258632+sachinkalambe1@users.noreply.github.com>
2023-01-25 10:50:55 +0530

    implicit by java

---
+++ implicit.java
@@ -0,0 +1,11 @@
+public class implicit
+{
+public static void main(String arg[])
+{
+byte a=2;
+//short s=a;
+//System.out.println(+s);
+long s=512342345;
+     a=(byte)s;
+System.out.println(+a);
+}}
