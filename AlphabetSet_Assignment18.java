1dc253bcb8b979727666ebbdb0a8aa9893427d26
sachinkalambe1 <118258632+sachinkalambe1@users.noreply.github.com>
2023-02-14 09:04:05 +0530

    assign

---
+++ AlphabetSet_Assignment18.java
@@ -0,0 +1,9 @@
+//Write a C program to print the alphabet set in decimal and character form.
+
+public class AlphabetSet_Assignment18 {
+  public static void main(String[] args) {
+    for (int i = 65; i <= 90; i++) {
+      System.out.println("Decimal: " + i + " Character: " + (char) i);
+    }
+  }
+}
