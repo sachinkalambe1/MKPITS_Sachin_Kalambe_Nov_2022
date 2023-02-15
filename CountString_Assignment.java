b8d88c8fcf5e5d44187e645e4eafb02467336317
Sachin kalambe <kalambesachin2000@gmail.com>
2023-02-16 00:26:01 +0530

    Count String in Assignment

---
+++ CountString_Assignment.java
@@ -0,0 +1,19 @@
+//Write a C program to copy a given string into another and count the number of characters copied.
+
+import java.util.*;
+class CountString_Assignment{
+public static void main(String[]args)
+{
+
+Scanner Sc=new Scanner(System.in);
+System.out.println("Enter String");
+String s1=Sc.nextLine();
+
+//System,out,println("Enter Second string");
+String s2=Sc.nextLine();
+strcpy(str2, str1);
+System.out.println(s2);
+strcpy(s2, s1);
+
+}
+}
