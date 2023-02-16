34b82731e8443463e7e39f3b5fcb0daddce1d73b
Sachin kalambe <kalambesachin2000@gmail.com>
2023-02-17 01:10:55 +0530

    Interface override in class

---
+++ Finaldemo.java
@@ -0,0 +1,39 @@
+interface one{
+
+void dis();
+void absmethod();
+}
+class two{
+
+void dis1(){
+int a=1;
+System.out.println(a);
+}
+void absmethod1(){
+
+System.out.println("Abract method Called");
+}
+class three extends two implements one
+{
+public void dis(){
+
+System.out.println("Display called");
+}
+public void absmethod()
+{
+
+System.out.println("ABS Called");
+}
+}
+
+class Finaldemo{
+
+public static void main(String[]args)
+{
+three s=new three();
+s.dis();
+s.absmethod();
+s.dis1();
+s.absmethod1();
+}
+}
