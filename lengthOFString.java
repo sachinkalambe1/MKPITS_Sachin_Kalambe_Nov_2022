c482fd96824acfea40043c38f48e0442cc6f3314
Sachin kalambe <kalambesachin2000@gmail.com>
2023-02-22 23:42:50 +0530

    length of string

---
+++ lengthOFString.java
@@ -0,0 +1,11 @@
+class lengthOFString{
+public static void main(String[]args)
+{
+String data="sachin";
+//char data[]={'s','a','c','h','i','n'};
+
+//String ab=new String(data);
+int len=data.length();
+System.out.println(len);
+}
+}
