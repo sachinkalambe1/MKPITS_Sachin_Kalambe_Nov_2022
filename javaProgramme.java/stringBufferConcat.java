e9da816fc302470bc26b7c23bff7b52ed74e81fc
Sachin kalambe <kalambesachin2000@gmail.com>
2023-03-01 23:58:28 +0530

    string buffer concat

---
+++ javaProgramme.java/stringBufferConcat.java
@@ -0,0 +1,13 @@
+class stringBufferConcat
+{
+public static void main(String[]args)
+{
+StringBuffer s=new String("Sachin");
+StringBuffer k=new String("Kalambe");
+
+StringBuffer sk=s.concat(k);
+Syatem.out.println(sk);
+
+
+}
+}
