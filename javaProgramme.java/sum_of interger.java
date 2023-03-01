c60bf6427745fa07b35076c16046f051169aee19
Sachin kalambe <kalambesachin2000@gmail.com>
2023-03-01 23:59:21 +0530

    sum of integer

---
+++ javaProgramme.java/sum_of interger.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class sum_of_interger{
+public static void main(String[]args)
+{
+
+Scanner Sc=new Scanner(System.in);
+System.out.println("Enter 1st integer number");
+int num1=Sc.nextInt();
+
+System.out.println("Enter 2nd integer number ");
+double num2=Sc.nextDouble();
+
+double sum=num1+num2;
+System.out.println(sum);
+
+}
+}
