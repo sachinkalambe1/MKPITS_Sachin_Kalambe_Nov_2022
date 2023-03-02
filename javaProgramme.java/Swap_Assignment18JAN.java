f10d6a312937368ae5c61168bb71dc482ac5cf5e
Sachin kalambe <kalambesachin2000@gmail.com>
2023-03-02 23:13:46 +0530

    Swap_assignment by java

---
+++ javaProgramme.java/Swap_Assignment18JAN.java
@@ -0,0 +1,14 @@
+class Swap_Assignment18JAN
+{
+public static void main(String[]args)
+{
+int a=2,b=3;
+System.out.println("A :"+a);
+System.out.println("B :"+b);
+a=a+b;
+b=a-b;
+a=a-b;
+System.out.println("Value After Swaping A  :"+a);
+System.out.println("Value After Swaping B  :"+b);
+}
+}
