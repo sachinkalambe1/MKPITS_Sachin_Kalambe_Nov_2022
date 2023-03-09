a1725bfd3a016dbeb28538c4937110e031c82e6b
Sachin kalambe <kalambesachin2000@gmail.com>
2023-03-09 22:49:50 +0530

    pattern by java

---
+++ javaProgramme.java/pattern_logic.java
@@ -0,0 +1,24 @@
+//Write a Java  program to print the result of the specified operations.
+//        -1 + 4 * 6
+//
+//        ( 35+ 5 ) % 7
+//
+//        14 + -4 * 6 / 11
+//
+//        2 + 15 / 6 * 1 - 7 % 2
+public class pattern_logic {
+    public static void main(String[] args) {
+
+        int result = -1+4*6;
+        System.out.println("result of opration : "+result);
+
+        int result1= ( 35+ 5 ) % 7;
+        System.out.println("result of opration : "+result1);
+
+        int result2 = 14 + -4 * 6 / 11;
+        System.out.println("result of opration : "+result2);
+
+        int result3=  2 + 15 / 6 * 1 - 7 % 2;
+        System.out.println("result of opration : "+result3);
+    }
+}
