2f67e72324edf44e56771b02e682a603394822b1
Sachin kalambe <kalambesachin2000@gmail.com>
2023-04-04 23:54:05 +0530

    Convert a string Lowercase To Uppercase

---
+++ javaProgramme.java/LowUperrcase.java
@@ -0,0 +1,11 @@
+//Write a java programe to convert string into Uppercase.
+import java.util.Scanner;
+
+public class LowUperrcase {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter any string");
+        String s=sc.nextLine();
+        System.out.println(s.toUpperCase());
+    }
+}
