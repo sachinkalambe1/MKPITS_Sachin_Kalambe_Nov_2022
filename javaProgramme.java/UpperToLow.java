6e51b8dac443517e5189e910fcc280b05abf674b
Sachin kalambe <kalambesachin2000@gmail.com>
2023-04-04 23:55:38 +0530

    Convert string Uppercase to Lowercase

---
+++ javaProgramme.java/UpperToLow.java
@@ -0,0 +1,11 @@
+//Write a java programe to convert string into Lowercase.
+import java.util.Scanner;
+
+public class UpperToLow {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter any String");
+        String s=sc.nextLine();
+        System.out.println(s.toLowerCase());
+    }
+}
