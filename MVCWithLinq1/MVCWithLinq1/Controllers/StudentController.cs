8fec82f2a7d74eae2c0f31bd9c2e168b0adce223
Sachin kalambe <kalambesachin2000@gmail.com>
2025-06-22 14:24:23 +0530

    Student Managemnet Project in ASP.NET MVC by using LINQ

---
+++ MVCWithLinq1/MVCWithLinq1/Controllers/StudentController.cs
@@ -0,0 +1,84 @@
+﻿using MVCWithLinq1.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCWithLinq1.Controllers
+{
+    public class StudentController : Controller
+    {
+        StudentDAL studentDAL = new StudentDAL();
+        public ViewResult DisplayStudents()
+        {
+            var Students = studentDAL.GetStudents(null);
+            
+            return View(Students);
+        }
+       public ViewResult DisplayStudent(int Sid)
+        {
+            var Student = studentDAL.GetStudent(Sid,true);
+                return View(Student);
+        }
+
+      
+        public ViewResult AddStudent()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public RedirectToRouteResult AddStudent(Student student,HttpPostedFileBase selectedFile)
+        {
+            if (selectedFile != null)
+            {
+                string phyicalPath = Server.MapPath("~/Uploads/");
+                if (!Directory.Exists(phyicalPath))
+                
+                    Directory.CreateDirectory(phyicalPath);
+                
+                selectedFile.SaveAs(phyicalPath+selectedFile.FileName);
+                student.Photo = selectedFile.FileName;
+
+            }
+            student.Status = true;
+            studentDAL.InsertStudent(student);
+
+            return RedirectToAction("DisplayStudents");
+
+        }
+        public ViewResult EditStudent(int Sid)
+        {
+            var Student = studentDAL.GetStudent(Sid, true);
+            TempData["Photo"]=Student.Photo;
+            return View(Student);
+        }
+        public RedirectToRouteResult UpdateStudent(Student student,HttpPostedFileBase selectedFile)
+        {
+            if (selectedFile != null)
+            {
+                string phyicalPath = Server.MapPath("~/Uploads/");
+                if (!Directory.Exists(phyicalPath))
+
+                    Directory.CreateDirectory(phyicalPath);
+
+                selectedFile.SaveAs(phyicalPath + selectedFile.FileName);
+                student.Photo = selectedFile.FileName;
+
+            }
+            else if (TempData["Photo"]!=null)
+                student.Photo = TempData["Photo"].ToString();
+
+            student.Status = true;
+            studentDAL.UpdateStudent(student);
+            return RedirectToAction("DisplayStudents");
+        }
+        public RedirectToRouteResult DeleteStudent(int Sid)
+        {
+            studentDAL.DeleteStudent(Sid);
+            return RedirectToAction("DisplayStudents");
+        }
+    }
+}
---
+++ MVCWithLinq1/MVCWithLinq1/Models/StudentDAL.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+
+namespace MVCWithLinq1.Models
+{
+
+    public class StudentDAL
+    {
+        MVCDBDataContext dc;
+        public StudentDAL() 
+        {
+            string ConStr = ConfigurationManager.ConnectionStrings["MVCDBConnectionString"].ConnectionString;
+            dc= new MVCDBDataContext(ConStr);
+        }
+      
+        public List<Student> GetStudents(bool? Status)
+        {
+            List<Student> students;
+            if (Status == null)
+            {
+                students = (from s in dc.Students select s).ToList();
+            }
+            else
+            {
+                students= (from s in dc.Students where s.Status == Status select s).ToList();
+            }
+            return students;
+        }
+        public Student GetStudent(int Sid, bool? Status)
+        {
+            Student students;
+
+            if (Status == null)
+            {
+                students = dc.Students.FirstOrDefault(s =>s.Sid==Sid);
+            }
+            else
+            {
+                students = dc.Students.FirstOrDefault(s=>s.Sid==Sid && s.Status==Status);
+            }
+
+            return students;
+        }
+      public void InsertStudent(Student student)
+        {
+            try
+            {
+                dc.Students.InsertOnSubmit(student);
+                dc.SubmitChanges();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            
+        }
+        public void UpdateStudent(Student student)
+        {
+            try
+            {
+                Student OldStudnet = dc.Students.SingleOrDefault(S => S.Sid == student.Sid);
+                OldStudnet.Name = student.Name;
+                OldStudnet.Class = student.Class;
+                OldStudnet.Fees = student.Fees;
+                OldStudnet.Photo = student.Photo;
+                OldStudnet.Status = student.Status;
+                dc.SubmitChanges();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            
+        }
+        public void DeleteStudent(int Sid)
+        {
+            try
+            {
+                Student OldStudent = dc.Students.SingleOrDefault(S => S.Sid == Sid);
+                dc.Students.DeleteOnSubmit(OldStudent);
+                dc.SubmitChanges();
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            
+        }
+
+
+    }
+}
